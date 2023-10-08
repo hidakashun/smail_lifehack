b7f4693489aeaae97ca2bcfffcdda4f08b02cb5a
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-10-08 09:20:16 +0000

    fix

--- app/models/lifehack.rb
+++ app/models/lifehack.rb
@@ -9,12 +9,12 @@ class Lifehack < ApplicationRecord
   has_many :lifehack_comments, dependent: :destroy
   belongs_to :user
 
-  def get_lifehack_image
+  def get_lifehack_image(width, height)
     unless lifehack_image.attached?
       file_path = Rails.root.join('app/assets/images/no_lifehack_image.jpg')
       lifehack_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    lifehack_image
+    lifehack_image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
