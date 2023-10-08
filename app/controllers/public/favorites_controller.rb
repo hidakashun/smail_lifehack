19ffe6e18aa5d8e756558031e9ad1f82e117ec34
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-10-08 05:03:54 +0000

    いいね、コメント機能非同期通信

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,15 +1,15 @@
 class Public::FavoritesController < ApplicationController
   def create
     lifehack = Lifehack.find(params[:lifehack_id])
-    favorite = current_user.favorites.new(lifehack_id: lifehack.id)
-    favorite.save
-    redirect_to request.referer
+    @favorite = current_user.favorites.new(lifehack_id: lifehack.id)
+    @favorite.save
+    render 'replace_btn'
   end
 
   def destroy
     lifehack = Lifehack.find(params[:lifehack_id])
-    favorite = current_user.favorites.find_by(lifehack_id: lifehack.id)
-    favorite.destroy
-    redirect_to request.referer
+    @favorite = current_user.favorites.find_by(lifehack_id: lifehack.id)
+    @favorite.destroy
+    render 'replace_btn'
   end
 end
--- app/controllers/public/lifehack_comments_controller.rb
+++ app/controllers/public/lifehack_comments_controller.rb
@@ -1,15 +1,14 @@
 class Public::LifehackCommentsController < ApplicationController
   def create
     lifehack = Lifehack.find(params[:lifehack_id])
-    comment = current_user.lifehack_comments.new(lifehack_comment_params)
-    comment.lifehack_id = lifehack.id
-    comment.save
-    redirect_to request.referer
+    @comment = current_user.lifehack_comments.new(lifehack_comment_params)
+    @comment.lifehack_id = lifehack.id
+    @comment.save
   end
 
   def destroy
-    LifehackComment.find_by(id: params[:id], lifehack_id: params[:lifehack_id]).destroy
-    redirect_to request.referer
+    @comment = LifehackComment.find(params[:id])
+    @comment.destroy
   end
 
   private
