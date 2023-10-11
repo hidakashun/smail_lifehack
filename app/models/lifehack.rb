8e12f5a5e1404b251371bd4af072a6a317e3255b
hidakashun <hidakashun60@gmail.com>
2023-10-11 12:07:57 +0000

    fix-13いいね機能

--- app/models/lifehack.rb
+++ app/models/lifehack.rb
@@ -1,9 +1,11 @@
 class Lifehack < ApplicationRecord
   #いいね機能
   has_many :favorites,dependent: :destroy
-  def favorited_by?(user)
-    favorites.where(user_id: user.id).exists?
-  end
+
+    def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+    end
+
   #コメント機能
   has_many :lifehack_comments, dependent: :destroy
   belongs_to :user
