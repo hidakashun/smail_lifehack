e0b49a56c641d69e6f41a61dfeb824c0e95ac31d
hidakashun <hidakashun60@gmail.com>
2023-10-11 10:22:00 +0000

    admin側作成開始

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
+    @lifehacks = Lifehack.all
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,9 @@
 class Admin::UsersController < ApplicationController
+  def index
+   @user = User.page(params[:page]).per(10)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
