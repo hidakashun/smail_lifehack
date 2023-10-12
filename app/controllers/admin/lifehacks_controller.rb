9f3571cc0f70c9b48055820c9c2b42242bfa3470
hidakashun <hidakashun60@gmail.com>
2023-10-12 10:21:01 +0000

    管理者投稿削除時フラッシュメッセージ追加

--- app/controllers/admin/lifehacks_controller.rb
+++ app/controllers/admin/lifehacks_controller.rb
@@ -9,6 +9,6 @@ class Admin::LifehacksController < ApplicationController
   def destroy
     @lifehack = Lifehack.find(params[:id])
     @lifehack.destroy
-    redirect_to admin_root_path
+    redirect_to admin_root_path, notice: "管理者権限により投稿を削除しました。"
   end
 end
