0d20906ad11b7b4b69e79bb561293bfd8b3ab3cf
hidakashun <hidakashun60@gmail.com>
2023-10-15 15:23:30 +0000

    画像選択時にエラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com',account_name: 'guest') do |user|
+    find_or_create_by!(email: 'guest@example.com',account_name: 'ゲストアカウント') do |user|
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
