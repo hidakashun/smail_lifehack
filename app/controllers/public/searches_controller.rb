4d8e0cb3f9037a1e4f8708748183d320896c2b72
hidakashun <hidakashun60@gmail.com>
2023-10-16 10:51:21 +0000

    Turbolinks無効

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,7 +5,6 @@ class Public::SearchesController < ApplicationController
     @content = params[:content1]  # 検索ワード
     @method = params[:method]  # 検索方法
 
-    # すべてのライフハックを取得し、後続の検索結果をフィルタリングするための基本となるデータを用意します
     @lifehacks = Lifehack.page(params[:page])
                          .per(10)
                          .order(created_at: :desc)
