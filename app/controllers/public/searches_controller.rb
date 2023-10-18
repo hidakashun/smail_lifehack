c50cad11f49359141d36cec667ba90de7bb1fca7
hidakashun <hidakashun60@gmail.com>
2023-10-18 03:34:29 +0000

    [fix]検索機能content

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,7 +2,7 @@ class Public::SearchesController < ApplicationController
   def search
     # フォームから送信された情報を受け取ります
     @model = params[:model]  # 検索モデル（ユーザーまたはライフハック）
-    @content = params[:content1]  # 検索ワード
+    @content = params[:keyword_content]  # キーワード検索ワード
     @method = params[:method]  # 検索方法
 
     @lifehacks = Lifehack.page(params[:page])
--- app/controllers/public/tagsearches_controller.rb
+++ app/controllers/public/tagsearches_controller.rb
@@ -1,7 +1,7 @@
 class Public::TagsearchesController < ApplicationController
   def search
     @model = Lifehack  #search機能とは関係なし
-    @word = params[:content2]  # 検索ワード
+    @word = params[:tagcontent]  # タグ検索ワード
     @lifehacks = Lifehack.where("tag LIKE?","%#{@word}%")
                          .page(params[:page]).per(10)
                          .where(is_draft: false)
