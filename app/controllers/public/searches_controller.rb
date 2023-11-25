a674d3fd98811707620f78170151f5c39781a994
hidakashun <hidakashun60@gmail.com>
2023-11-25 08:19:45 +0000

    [fix]キーワード検索のページネーション修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,20 +5,19 @@ class Public::SearchesController < ApplicationController
     @content = params[:keyword_content] # キーワード検索ワード
     @method = params[:method] # 検索方法
 
-    @lifehacks = Lifehack.page(params[:page])
-                         .per(10)
-                         .order(created_at: :desc)
-                         .where(is_draft: false)
-
     @records = if @model == 'user'
                  # ユーザーモデルで検索を実行します
                  User.search_for(@content, @method)
                      .order(created_at: :desc)
+                     .page(params[:page])
+                     .per(10)
                else
                  # ライフハックモデルで検索を実行します
                  Lifehack.search_for(@content, @method)
                          .order(created_at: :desc)
                          .where(is_draft: false)
+                         .page(params[:page])
+                         .per(10)
                end
   end
 end
